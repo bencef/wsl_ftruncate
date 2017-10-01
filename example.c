7897f22559a253e3effe43993cb79f4b231dd373
Bence Fabian <begnoc@gmail.com>
2017-10-01 12:38:39 +0200

    file creation

--- example.c
+++ example.c
@@ -17,15 +17,36 @@ int create_file(size_t);
 
 int create_file(size_t size)
 {
-  char *filename = mktemp("/tmp/wsl_ftruncate.XXXXXX");
-  int fd = open(filename, O_WRONLY|O_CREAT, 0644);
+  char template[] = "/tmp/wsl_ftruncate.XXXXXX";
+  char *filename;
+  int fd;
+  int error;
+  char *content;
+  ssize_t written;
+
+  filename = mktemp(template);
+  fd = open(filename, O_WRONLY|O_CREAT, 0644);
   if(fd == -1) {
-    int error = errno;
+    error = errno;
     fprintf(stderr, "Couldn't create %s with size %d: %s\n",
             filename,
             size,
             strerror(error));
+    return fd;
   }
+  printf("File created: %s\n", filename);
+  if(size > 0) {
+    content = malloc(size);
+    content = memset(content, 'A', size);
+    written = write(fd, content, size);
+    if(written == -1) {
+      error = errno;
+      fprintf(stderr, "Couldn't write into %s: %s\n",
+              filename,
+              strerror(error));
+    }
+  }
+  unlink(filename); // will delete after close
   return fd;
 }
 
